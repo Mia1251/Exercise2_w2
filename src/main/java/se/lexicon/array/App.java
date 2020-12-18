822d71dd382611d7dc88d6f595f169c6a249eb12
Mia1251 <svanteomax@gmail.com>
2020-12-18 13:50:13 +0100

    Exercise2_w2

---
+++ src/main/java/se/lexicon/array/App.java
@@ -0,0 +1,15 @@
+package se.lexicon.array;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+        int[] indexOf = {2,4,5,6,9};
+
+        int indexFound = Arrays.binarySearch(indexOf, (9));
+        System.out.println(indexFound);
+        int indexNotFound = Arrays.binarySearch(indexOf, (-1));
+        System.out.println(indexNotFound);
+    }
+}
